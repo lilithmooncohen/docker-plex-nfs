735b3fe7d2290245fb17aa8abe9d41f26f0634dc
ryanckoch <ryanckoch@gmail.com>
2015-04-10 10:25:04 -0600

    Changed to connect to remote nfs server rather than connect to container. Adjusted README.

--- README.md
+++ README.md
@@ -4,9 +4,5 @@ Docker NFS Server
 Usage
 ----
 ```bash
-docker run -d --name nfs cpuguy83/nfs-server /path/to/share /path/to/share2 /path/to/shareN
-```
-
-```bash
-docker run -d --name nfs-client --link nfs:nfs cpuguy83/nfs-client /path/on/nfs/server:/path/on/client
+docker run -d --name nfs-client ryanckoch/nfs-client nfs_server.domain.com:/path/on/nfs/server:/path/on/client
 ``` 
--- nfs-client.sh
+++ nfs-client.sh
@@ -8,13 +8,14 @@ targets=()
 rpcbind
 
 for mnt in "${mounts[@]}"; do
-  src=$(echo $mnt | awk -F':' '{ print $1 }')
-  target=$(echo $mnt | awk -F':' '{ print $2 }')
+  server=$(echo $mnt | awk -F':' '{ print $1 }')
+  src=$(echo $mnt | awk -F':' '{ print $2 }')
+  target=$(echo $mnt | awk -F':' '{ print $3 }')
   targets+=("$target")
 
   mkdir -p $target
 
-  mount -t nfs -o proto=tcp,port=2049 ${NFS_PORT_2049_TCP_ADDR}:${src} ${target}
+  mount -t nfs -o proto=tcp,port=2049 $server:${src} ${target}
 done
 
 exec inotifywait -m "${targets[@]}"
